7dd62c732a175a3557ece7c3f662368e9dea9750
dhena201 <dhenaaun@gmail.com>
2016-03-10 21:21:42 +0700

    CRUD
    Selesai CRUD untuk modul dosen, prodi dan matkul

--- application/models/Prodi_model.php
+++ application/models/Prodi_model.php
@@ -3,15 +3,74 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Prodi_model extends CI_Model {
 
-	public $db_tabel = 'prodi';
+	var $table = 'prodi';
+    var $column = array('id_prodi','nama_prodi'); //set column field database for order and search
+    var $order = array('id_prodi' => 'asc'); // default order
 	public function __construct(){
 		parent::__construct();
+		$this->load->database();
 	}
-
-	public function select_all(){
-		$sql = 'select * from prodi order by prodi.nama_prodi';
-		$data = $this->db->query($sql);
-		return $data->result_array();
-	}
+	private function _get_datatables_query(){
+         
+        $this->db->from($this->table);
+ 
+        $i = 0;
+     
+        foreach ($this->column as $item) // loop column
+        {
+            $column[$i] = $item; // set column array variable to order processing
+            $i++;
+        }
+         
+        if(isset($_POST['order'])) // here order processing
+        {
+            $this->db->order_by($column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        }
+        else if(isset($this->order))
+        {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+	function get_datatables(){
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+ 
+    function count_filtered(){
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all(){
+        $this->db->from($this->table);
+        return $this->db->count_all_results();
+    }
+ 
+    public function get_by_id($id){
+        $this->db->from($this->table);
+        $this->db->where('id_prodi',$id);
+        $query = $this->db->get();
+ 
+        return $query->row();
+    }
+ 
+    public function save($data){
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+ 
+    public function update($data){
+    	$this->db->where('id_prodi',$data['id_prodi']);
+        $this->db->update($this->table, $data);
+        return $this->db->affected_rows();
+    }
+ 
+    public function delete_by_id($id){
+        $this->db->where('id_prodi', $id);
+        $this->db->delete($this->table);
+    }
 }
 ?>
--- application/views/viewMatkul.php
+++ application/views/viewMatkul.php
@@ -11,8 +11,12 @@
             <div class="clearfix"></div>
         </div>
         <div class="portlet-body">
+            <button class="btn btn-success" onclick="add_data()"><i class="fa fa-plus"></i> Tambah</button>
+            <button class="btn btn-default" onclick="reload_table()"><i class="fa fa-refresh"></i> Reload</button>
+            <br/>
+            <br/>
             <div class="table-responsive">
-                <table id="example-table" class="table table-striped table-bordered table-hover table-green">
+                <table id="table" class="table table-striped table-bordered table-hover table-green">
                     <thead>
                         <tr>
                             <th>Mata Kuliah</th>
@@ -23,24 +27,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                    <?php 
-                    foreach($matakuliah as $row){
-                    ?>
-                        <tr>
-                            <td><?php echo $row['nama_kuliah']; ?></td>
-                            <td><?php echo $row['sks']; ?></td>
-                            <td><?php echo $row['semester']; ?></td>
-                            <td><?php echo $row['nama_prodi']; ?></td>
-                            <td>
-                                <a href=""><i class="fa fa-edit" data-toggle="modal" data-target="#flexModal"></i></a>&nbsp
-                                <a href="" onclick="return confirm('Anda Yakin Menghapus Data ini?')"><i class="fa fa-trash-o"></i></a>
-                            </td>
-                        </tr>
-                    <?php } ?>
                     </tbody>
-                    <!-- Flex Modal -->
-                    
-                    <!-- /.modal -->
                 </table>
             </div>
         </div>
@@ -48,22 +35,254 @@
     <!-- /.portlet -->
 </div>
 <!-- /.col-lg-6 -->
-<div class="modal modal-flex fade" id="flexModal" tabindex="-1" role="dialog" aria-labelledby="flexModalLabel" aria-hidden="true">
+<script type="text/javascript">
+ 
+var save_method; //for save method string
+var table;
+ 
+$(document).ready(function() {
+ 
+    //datatables
+    table = $('#table').DataTable({
+ 
+        // "processing": true, //Feature control the processing indicator.
+        // "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo base_url('mata_kuliah/ajax_list')?>",
+            "type": "GET"
+        },
+        "columns" :[
+            {"data" : "nama_kuliah"},
+            {"data" : "sks"},
+            {"data" : "semester"},
+            {"data" : "nama_prodi"},
+            {"data":"id",render:function(data){
+                var btn = "<a class='btn btn-sm btn-primary' href='javascript:void()' title='Edit' onclick='edit_data("+data+")'><i class='fa fa-edit'></i></a>&nbsp<a class='btn btn-sm btn-danger' href='javascript:void()' title='Hapus' onclick='delete_data("+data+")'><i class='fa fa-trash-o'></i></a>";
+                return btn;
+            }
+            }
+        ],
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        {
+            "targets": [ -1 ], //last column
+            "orderable": false, //set not orderable
+        },
+        ],
+ 
+    });
+ 
+ 
+    //set input/textarea/select event when change value, remove class error and remove text help block
+    $("input").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("textarea").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("select").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+ 
+});
+ 
+function add_data()
+{
+    save_method = 'add';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+    $('#modal_form').modal('show'); // show bootstrap modal
+    $('.modal-title').text('Tambah Data'); // Set Title to Bootstrap modal title
+
+}
+ 
+function edit_data(id)
+{
+    save_method = 'update';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+ 
+    //Ajax Load data from ajax
+    $.ajax({
+        url : "<?php echo base_url('mata_kuliah/ajax_edit/')?>/" + id,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+ 
+            $('[name="id"]').val(data.id_kuliah);
+            $('[name="nama_kuliah"]').val(data.nama_kuliah);
+            $('[name="sks"]').val(data.sks);
+            $('[name="semester"]').val(data.semester);
+            $('[name="id_prodi"]').val(data.id_prodi);
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Ubah Data'); // Set title to Bootstrap modal title
+ 
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    });
+}
+ 
+function reload_table()
+{
+    table.ajax.reload(); //reload datatable ajax
+}
+ 
+function save()
+{
+    $('#btnSave').text('saving...'); //change button text
+    $('#btnSave').attr('disabled',true); //set button disable
+    var url;
+ 
+    if(save_method == 'add') {
+        url = "<?php echo base_url('mata_kuliah/ajax_add')?>";
+    } else if(save_method == 'update'){
+        url = "<?php echo base_url('mata_kuliah/ajax_update')?>";
+    }
+ 
+    // ajax adding data to database
+    $.ajax({
+        url : url,
+        type: "POST",
+        data: $('#form').serialize(),
+        dataType: "JSON",
+        success: function(data)
+        {
+ 
+            if(data.status) //if success close modal and reload ajax table
+            {
+                $('#modal_form').modal('hide');
+                reload_table();
+            }
+            else
+            {
+                for (var i = 0; i < data.inputerror.length; i++)
+                {
+                    $('[name="'+data.inputerror[i]+'"]').parent().parent().addClass('has-error'); //select parent twice to select div form-group class and add has-error class
+                    $('[name="'+data.inputerror[i]+'"]').next().text(data.error_string[i]); //select span help-block class set text error string
+                }
+            }
+            $('#btnSave').text('save'); //change button text
+            $('#btnSave').attr('disabled',false); //set button enable
+ 
+ 
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error adding / update data');
+            $('#btnSave').text('save'); //change button text
+            $('#btnSave').attr('disabled',false); //set button enable
+ 
+        }
+    });
+}
+ 
+function delete_data(id)
+{
+    if(confirm('Are you sure delete this data?'))
+    {
+        // ajax delete data to database
+        $.ajax({
+            url : "<?php echo base_url('mata_kuliah/ajax_delete')?>/"+id,
+            type: "POST",
+            dataType: "JSON",
+            success: function(data)
+            {
+                //if success reload ajax table
+                $('#modal_form').modal('hide');
+                reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Error deleting data');
+            }
+        });
+ 
+    }
+}
+ 
+</script>
+<!-- Bootstrap modal -->
+<div class="modal fade" id="modal_form" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="flexModalLabel">Flex Admin Styled Modal</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title">Form Mata Kuliah</h3>
             </div>
-            <div class="modal-body">
-
+            <div class="modal-body form">
+                <form action="#" id="form" class="form-horizontal">
+                    <input type="hidden" value="" name="id"/>
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Nama Mata Kuliah</label>
+                            <div class="col-md-9">
+                                <input name="nama_kuliah" placeholder="Nama Mata Kuliah" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Jumlah SKS</label>
+                            <div class="col-md-9">
+                                <select name="sks" class="form-control">
+                                    <option value="">--Pilih Jumlah SKS--</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Semester</label>
+                            <div class="col-md-9">
+                                <select name="semester" class="form-control">
+                                    <option value="">--Pilih Semester--</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                </select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Program Studi</label>
+                            <div class="col-md-9">
+                                <select name="id_prodi" class="form-control">
+                                    <option value="">--Pilih Program Studi--</option>
+                                    <?php foreach($prodi as $row){ 
+                                    echo "<option value='$row[id_prodi]'>$row[nama_prodi]</option>";
+                                    }
+                                    ?>
+                                </select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-green">Save changes</button>
+                <button type="button" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
             </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- End Bootstrap modal -->
